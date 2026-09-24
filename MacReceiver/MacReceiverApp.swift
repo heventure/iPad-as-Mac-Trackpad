e058b64d8cb1c27d654c7b016c2964f82c85f24a
HHWY Inc. <richardhughes@live.cn>
2026-09-24 13:59:00 +0800

    Resize Mac receiver utility window

--- MacReceiver/MacReceiverApp.swift
+++ MacReceiver/MacReceiverApp.swift
@@ -1,2 +1,13 @@
 import SwiftUI
-@main struct MacReceiverApp:App{@StateObject private var receiver=MacPeerReceiver();var body:some Scene{WindowGroup{MacReceiverView(receiver:receiver).frame(minWidth:520,minHeight:380)}.windowResizability(.contentSize)}}
+
+@main struct MacReceiverApp:App {
+ @StateObject private var receiver=MacPeerReceiver()
+
+ var body:some Scene {
+  WindowGroup {
+   MacReceiverView(receiver:receiver)
+    .frame(minWidth:440,minHeight:330)
+  }
+  .windowResizability(.contentSize)
+ }
+}
